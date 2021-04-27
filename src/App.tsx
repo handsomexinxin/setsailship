ac6c835f2777af87a26a2760a2fe7062051a70fa
何鑫鑫 <hx17693012635@163.com>
2021-04-27 20:13:51 +0800

    create

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,6 +15,14 @@ function App() {
         >
           Learn React
         </a>
+        <h1>hello world</h1>
+        <h2>hello world</h2>
+        <h3>hello world</h3>
+        <h4>hello world</h4>
+        <h5>hello world</h5>
+        <h6>hello world</h6>
+        <code>const a = b</code>
+
       </header>
     </div>
   );
